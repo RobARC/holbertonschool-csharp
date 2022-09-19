91d746ff2e7802273f20d93ce860adccbe4a6d9a
RobARC <rrondonc@gmail.com>
2022-09-19 17:27:05 -0500

    update file 0-initialize_new_project.sh

--- 0x00-csharp-hello_world/0-initialize_new_project.sh
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir 0-new_project
-cd 0-new_project
-dotnet new console
-echo "The template 'Console Application' was created successfully."
-exit
+dotnet new console -o 0-new_project
+
