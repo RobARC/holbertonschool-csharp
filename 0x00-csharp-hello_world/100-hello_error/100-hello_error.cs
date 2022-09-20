4b21e4bf8d3323e7219d29316215765ac4655c00
RobARC <rrondonc@gmail.com>
2022-09-20 17:39:33 -0500

    Uploading Task 12

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+            Console.Error.WriteLine(str);
+            Environment.Exit(1);
+        }
+    }
+}
