cf0962548af5f90575cff67821b4120edee482a0
RobARC <rrondonc@gmail.com>
2022-09-21 15:54:24 -0500

    Update file 1-last_digit.cs

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -6,18 +6,16 @@
         {
             Random rndm = new Random();
             int number = rndm.Next(-10000, 10000);
-            Console.WriteLine("The last digit of" + number + " is ");
-            if(number > 5)
+            Console.WriteLine($"The last digit of {number} is {number %10} and is ");
+            if(number % 10 > 5)
             {
                 Console.WriteLine("greater than 5");    
             }
             else if(number % 10 <= 5 && number % 10 != 0)
             {
-                Console.WriteLine("and is less than 6 and not 0");
+                Console.WriteLine("less than 6 and not 0");
             }
             else 
-            {
-                Console.WriteLine(" and is 0");
-            }
+                Console.WriteLine("0");
         }
     }
