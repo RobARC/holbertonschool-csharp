5178c022db62e93a0aa07494b1c559dcb283ca70
RobARC <rrondonc@gmail.com>
2022-09-28 09:37:00 -0500

    Update task 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(char letter1 = 'a'; letter1 <= 'z'; letter1++)
+                Console.Write(letter1);
+        }
+    }
+}
