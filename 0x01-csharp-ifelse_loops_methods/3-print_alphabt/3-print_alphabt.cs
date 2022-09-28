4c2eafab2b917213f3b76d7b004b7cc99703b850
RobARC <rrondonc@gmail.com>
2022-09-28 09:50:35 -0500

    Uploading Task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(char letter = 'a'; letter <= 'z'; letter++)
+            {
+                if (letter == 'q' || letter == 'e')
+                    continue;
+                Console.Write(letter);
+            }
+        }
+    }
+}
