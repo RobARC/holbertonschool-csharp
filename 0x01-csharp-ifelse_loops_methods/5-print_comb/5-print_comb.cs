02120cbb72631b55288ecbff9da178e182745d40
RobARC <rrondonc@gmail.com>
2022-09-28 11:35:18 -0500

    Uploading task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                if(i < 99)
+                {
+                    Console.Write($"{i:d2}, ");
+                }
+                else
+                    Console.Write(i);    
+            }
+           
+        }
+    }
+}
