272cb80024c25c6fb4812f15dde3950625a4e57f
RobARC <rrondonc@gmail.com>
2022-12-04 14:28:50 -0500

    Uploading Task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        string character = "\\"; 
+
+         if(length == 0)
+        {
+            Console.WriteLine("$");
+        }
+         else 
+        {
+           for (int i = length; i > 0; i--)
+           {
+            Console.WriteLine(new String(' ', length -i)+ character + "$");
+            
+           }
+           Console.WriteLine("$");
+        }
+
+    }
+}
