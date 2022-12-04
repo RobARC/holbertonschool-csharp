4adefc25ba9764d502c253477fa5708275f70bb6
RobARC <rrondonc@gmail.com>
2022-12-04 13:30:57 -0500

    Uploading task 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        string character = "_";
+
+        if(length == 0)
+        {
+            Console.WriteLine("$");
+        }
+        else 
+        {
+           for (int i = 0; i< length; i++)
+           {
+            Console.Write(character);
+           }
+           Console.WriteLine("$");
+        }
+    }
+}
