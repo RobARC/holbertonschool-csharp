04f2f664a12ba254a49f0d1e4fd81787c2a17665
RobARC <rrondonc@gmail.com>
2022-12-04 12:42:41 -0500

    Uploading task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int digit = number % 10;
+        if (digit < -1)
+        {
+            Console.Write(digit * -1);
+            return (digit * -1);
+        }
+        Console.Write(digit);
+        return digit;
+
+    }
+}
