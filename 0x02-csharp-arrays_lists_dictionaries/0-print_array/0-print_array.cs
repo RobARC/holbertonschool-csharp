11cb378c9272455fd9be1c063a966453718d1b1d
RobARC <rrondonc@gmail.com>
2022-12-10 10:09:15 -0500

    Uploading task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size){
+        
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        int [] myArray = new int[size];
+        if (size == 0){
+            Console.WriteLine("");
+        } 
+        else 
+        {
+             for(int i = 0; i < size; i++)
+             {
+                 myArray[i] = i;
+                 if (i != myArray.Length -1)
+                 {
+                    Console.Write(myArray[i] + " ");
+                 }
+                 else{
+                    Console.Write(myArray[i]);
+                 }
+             }
+             Console.WriteLine("");
+        }
+         return myArray;
+    }
+}
