f11e2d76ff0d9849607dad90a083c60b70139fe8
RobARC <rrondonc@gmail.com>
2022-12-10 23:12:30 -0500

    Uploading task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array 
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("");
+            return;
+        }
+        for( int i = array.Length -1;i >= 0; i-- )
+        {
+            if (i != 0)
+            {
+                Console.Write(array[i] + " ");
+            }
+            else 
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
