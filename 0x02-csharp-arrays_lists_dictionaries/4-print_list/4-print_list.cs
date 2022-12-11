7cffa8f1735b72d5366eadcee6db3d53f7799647
RobARC <rrondonc@gmail.com>
2022-12-11 09:47:04 -0500

    Uploading task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0 )
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        List<int> myList = new List<int>(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            myList.Add(i);
+            if (i == size)
+            {
+                  Console.WriteLine(i);
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }  
+        Console.WriteLine("");   
+        return myList;
+    }
+}
