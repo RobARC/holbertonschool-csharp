687e83302bdefcb563cc7924a85fdaed464b17e2
RobARC <rrondonc@gmail.com>
2022-12-12 20:47:09 -0500

    Uploading Task 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int Counter = 0;
+        foreach(KeyValuePair<string, string> pair in myDict)
+        {
+            Counter += 1;
+        }
+        return Counter;
+    }
+}
