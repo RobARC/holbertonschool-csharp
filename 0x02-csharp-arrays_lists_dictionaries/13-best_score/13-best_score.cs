15a6267ef514f4abefde3a0c8f9e03025dcbada7
RobARC <rrondonc@gmail.com>
2022-12-21 14:17:20 -0500

    Uploading task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            string key = null;
+            int myValue = 0;
+
+            if (myList == null || myList.Count == 0){
+                return "None";
+            }
+
+            foreach (KeyValuePair<string, int> kvp in myList)
+            {
+                if(kvp.Value >= myValue)
+                {
+                    key = kvp.Key;
+                    myValue = kvp.Value;
+                }
+            }
+
+            return key;
+        }
+    }
+
