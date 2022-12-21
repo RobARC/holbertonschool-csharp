83e34d43b5dd93dfc153c0de05d83f7e2fd512e1
RobARC <rrondonc@gmail.com>
2022-12-21 11:36:45 -0500

    Uploading task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            List<string> myList = new List<string>();
+            Dictionary<string, string> myNewDict = new Dictionary<string, string>();
+
+            foreach (KeyValuePair<string, string> pair in myDict)
+            {
+                myList.Add(pair.Key);
+            }
+            myList.Sort();
+            foreach (var key in myList)
+            {
+                myNewDict[key] = myDict[key];
+            }
+
+            foreach(KeyValuePair<string, string> pair in myNewDict)
+            {
+                Console.WriteLine($"{pair.Key} : {pair.Value}");
+            }
+        }
+    }
+
