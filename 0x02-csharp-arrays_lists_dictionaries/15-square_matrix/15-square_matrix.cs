786d18c90c28b838c30739d580a8346d2cb8ffec
RobARC <rrondonc@gmail.com>
2022-12-23 08:45:20 -0500

    Uploading task 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Matrix
+    {
+        public static int[,] Square(int[,] myMatrix)
+        {
+           int[,] square = (int[,])myMatrix.Clone();
+
+           for (int i = 0; i < myMatrix.GetLength(0); i++)
+                for (int j = 0; j < myMatrix.GetLength(1); j++)
+                {
+                    square[i, j] = (int)Math.Pow(square[i, j], 2);
+                }    
+            return square;    
+        }
+    }
