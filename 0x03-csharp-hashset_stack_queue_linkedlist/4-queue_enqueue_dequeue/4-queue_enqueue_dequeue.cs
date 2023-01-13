79af54b42ab8b9355fba102ab0310d04e60957d4
RobARC <rrondonc@gmail.com>
2023-01-13 08:46:52 -0500

    Uploading Task 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Queue<string> queueCopy = new Queue<string>(aQueue.ToArray());
+
+        var check = true;
+        var aux = "";
+
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count > 0)
+            Console.WriteLine("First item: {0}", queueCopy.Dequeue());
+        else
+            Console.WriteLine("Queue is empty");
+        
+        aQueue.Enqueue(newItem);
+
+        if(aQueue.Contains(search))
+            Console.WriteLine($"Queue contains \"{search}\": True");
+        else
+            Console.WriteLine($"Queue contains \"{search}\": False");
+
+        while (check)
+        {
+            aux= aQueue.Dequeue();
+            if( String.Equals(search, aux))
+                check = false;
+            
+        }
+
+        return aQueue;
+    }
+}
