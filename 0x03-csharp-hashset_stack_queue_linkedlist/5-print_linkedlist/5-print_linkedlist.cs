4c6ea9dcc85124ad06ccd875759ab4a6125cd15c
RobARC <rrondonc@gmail.com>
2023-01-13 09:42:30 -0500

    Uploading task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        if (size < 0)
+            return myLList;
+
+        for(int i = 0; i < size; i++)
+        {
+            myLList.AddFirst(i);
+            Console.WriteLine(i);
+        }
+
+        return myLList;
+    
+    }
+}
