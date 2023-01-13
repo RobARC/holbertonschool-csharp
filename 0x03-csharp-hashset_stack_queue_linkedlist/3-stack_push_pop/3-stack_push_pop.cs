3961dc765a77e732e4a47497a9057fe5f88a90a3
RobARC <rrondonc@gmail.com>
2023-01-12 22:30:34 -0500

    uploading task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        string aux = "";
+        bool check = true;
+       
+        Console.WriteLine("\nNumber of items: {0}", aStack.Count);
+
+        if (aStack.Count > 0)
+            Console.WriteLine("\nTop item: {0}", aStack.Peek());
+        else
+            Console.WriteLine("Stack is empty");
+
+        if(aStack.Contains(search))
+            Console.WriteLine($"\nStack contains \"{search}\": True");
+        else
+            Console.WriteLine($"\nStack contains \"{search}\": False");
+
+        while (check)
+        {
+            aux = aStack.Peek();
+            if(String.Equals(search, aux))
+                check = false;
+            aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+
+    }
+}
