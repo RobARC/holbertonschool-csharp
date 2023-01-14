3e48e955c043cd8554f8672d22811c18f086ae36
RobARC <rrondonc@gmail.com>
2023-01-14 14:15:33 -0500

    Uploading task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        
+        LinkedListNode<int> current = myLList.First;
+        if(index == 0)
+        {
+            myLList.RemoveFirst();
+            return;
+        }
+        if (index == myLList.Count -1)
+        {
+            myLList.RemoveLast();
+            return;
+        }
+
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            if(i == index)
+            {
+                myLList.Remove(current);
+                break;
+            }
+            current = current.Next;
+        }
+        
+        return;
+    }
+}
