31d5112485999734c70e8d99612d2e22ce63a6d0
RobARC <rrondonc@gmail.com>
2023-01-14 11:07:17 -0500

    Uploading task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+         int count = 0;
+        foreach(var item in myLList)
+        {
+             //Console.WriteLine(item);
+            if (count == n)
+            {
+                return item;
+            }
+            count++;
+        }
+        return 0;
+    }
+}
