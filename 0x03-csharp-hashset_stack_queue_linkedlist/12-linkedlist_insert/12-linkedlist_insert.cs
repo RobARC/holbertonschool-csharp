44c1f3b19d1524ceec2df6cee12389fe4fe53d46
RobARC <rrondonc@gmail.com>
2023-01-14 13:37:23 -0500

    Uploading task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+
+        if (myLList.First.Value >= n)
+        {
+            myLList.AddFirst(n);
+            return myLList.First;
+        }
+
+        foreach(var item in myLList)
+        {
+            if(item < n )
+            {
+                current = current.Next;
+                if(current.Next == null)
+                {
+                    myLList.AddLast(n);
+                    return myLList.Last;
+                }
+            }
+        }
+        myLList.AddBefore(current, n);
+        return current.Previous;
+    }
+}
