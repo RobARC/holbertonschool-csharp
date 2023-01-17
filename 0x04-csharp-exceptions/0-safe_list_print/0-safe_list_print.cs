bebc22a4f9b6778a915dce018644182398d7964f
RobARC <rrondonc@gmail.com>
2023-01-16 19:24:00 -0500

    Uploading task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int counter = n;
+        try
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            return counter;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return myList.Count;
+
+        }
+       
+    }
+}
