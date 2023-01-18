bf12b868d31f242e23c1059ba2208999826c19d5
RobARC <rrondonc@gmail.com>
2023-01-18 12:08:57 -0500

    Uploading task 2

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog 
+{
+    public string name; 
+    public float age;
+    public string owner; 
+    public Rating rating;
+
+    public Dog(string dname, float dage, string downer, Rating drating)
+    {
+        name = dname;
+        age = dage;
+        owner = downer;
+        rating = drating;
+    }
+    
+}
