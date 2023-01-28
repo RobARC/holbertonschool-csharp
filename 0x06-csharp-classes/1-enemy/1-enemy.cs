dc201accf9abd317a6528d7d433de499d1bec071
RobARC <rrondonc@gmail.com>
2023-01-28 10:57:59 -0500

    Uploading task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Defines a Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Defines a Zombie.</summary>
+        public int health;
+
+        ///<summary>Initialize a new instance of <see cref="Zombie"/>.</summary>
+        public Zombie()
+        {
+            health = 0;
+        } 
+    }
+}
+
