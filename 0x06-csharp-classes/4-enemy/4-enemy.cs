74b0ad1474afdcf8469651e543c9984844d5cdcb
RobARC <rrondonc@gmail.com>
2023-01-28 11:58:18 -0500

    Uploading task 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Defines a Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Zombie Health</summary>
+        private int health;
+        ///<summary>Zombie name</summary>
+        private string name = "(No name)";
+
+        ///<summary>Constructor Method Initialize a new instance of <see cref="Zombie"/>.</summary>
+        public Zombie()
+        {
+            health = 0;
+        } 
+        /*<summary>You can write the constructor method this way:
+            public Zombie() => health = 0;
+         </summary>*/
+
+        ///<summary>New constructor method Initialize a new instance of <see cref="Zombie"/>.</summary>
+         public Zombie(int value)
+         {
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            } 
+            else
+            {
+                health = value;
+            }
+         }
+         
+         /// <summary>Gets or sets the name of the zombie.</summary>
+          public string Name { get { return name; } set { name = value; } }
+        ///<summary>Public method return health of the zombie.</summary>
+         public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
