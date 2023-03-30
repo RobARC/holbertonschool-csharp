b9dd6bc352bc5923db3214d37f88479e3bc86b36
RobARC <rrondonc@gmail.com>
2023-03-30 09:32:13 -0500

    Adding comments in xml format

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,9 +1,18 @@
-﻿namespace MyMath;
-public class Operations
-{
-    public static int Add(int a, int b)
-    {
-        return a + b;
-    }
+﻿using System;
 
+namespace MyMath
+{
+    public class Operations
+    {
+        ///<summary>
+        /// Method to Add two integers numbers
+        ///</summary>
+        ///<param name="a">first integer</param>
+        ///<param name="b">second integer</param>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
 }
+
