803a9b87f1632747ac7be8266f7044bb1fa85556
RobARC <rrondonc@gmail.com>
2023-03-30 09:33:00 -0500

    Adding comments in xml format

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -12,13 +12,20 @@ public class Tests
     }
 
     [Test]
+    /// <summary>
+    /// Return sum of a + b positive numbers
+    /// </summary>
+
     public void returnSumAB([Values(1, 2, 3)] int a, [Values(4, 5, 6)] int b)
     {
         int output =  Operations.Add(a, b);
         Assert.AreEqual(output, a + b);
     }
 
-      [Test]
+    [Test]
+    /// <summary>
+    /// Return sum of a + b Negative numbers
+    /// </summary>
     public void returnNegativeSumAB([Values(-1, -2, -3)] int a, [Values(-4, -5, -6)] int b)
     {
         int output =  Operations.Add(a, b);
